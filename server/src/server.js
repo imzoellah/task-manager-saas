a4e44e589bf921a7d3717ce37f2744280920d0ca
Amna <amna.bibi818181@gmail.com>
2026-05-28 17:47:28 +0500

    fix frontend path

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
@@ -11,23 +12,32 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// TEST ROUTE
 app.get("/ping", (req, res) => {
   res.send("pong");
 });
 
-// ROUTES
+// API ROUTES
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
+// SERVE FRONTEND
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
 // CONNECT DB + START SERVER
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
 
     const PORT = process.env.PORT || 5000;
 
     app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+      console.log(`Server running on ${PORT}`);
     });
   })
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
